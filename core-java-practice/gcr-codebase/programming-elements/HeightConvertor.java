26a2f2b58ef5ba4f877ce75fd0908b4f529b6eb7
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2025-12-18 18:51:34 +0530

    [Rishabh]Add. Your height in cm,ft and in

---
+++ core-java-practice/gcr-codebase/programming-elements/HeightConvertor.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class HeightConvertor {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter height in cm ");
+		double height_in_cm = sc.nextDouble();
+		double inches = height_in_cm / 2.54; // 1 inch = 2.54 cm
+        double feet = inches / 12; // 1 foot = 12 inches
+        System.out.println("Your Height in cm is " + height_in_cm + "while in feet is " + feet + " and in inches is " + inches);
+        sc.close();
+	}
+}
