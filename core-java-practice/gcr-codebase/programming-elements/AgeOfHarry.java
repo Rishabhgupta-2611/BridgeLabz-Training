224d79df433b1f5b33084aebfcf7186b20b1195b
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2025-12-18 16:23:48 +0530

    [Rishabh]Add. Age of Harry

---
+++ core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
@@ -0,0 +1,12 @@
+
+public class AgeOfHarry {
+
+	public static void main(String[] args) {
+		int CurrYear = 2024;
+		int DOB = 2000;
+		int age = CurrYear - DOB;
+		System.out.println("Harry's age in 2024 is "+ age);		
+
+	}
+
+}
