5304b2c5f1830c69e5397fd257a722f1903e0741
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2025-12-19 11:20:53 +0530

    [Rishabh]Refactor.Input Km To Miles

--- core-java-practice/gcr-codebase/programming-elements/UserInputKmToMiles.java
+++ core-java-practice/gcr-codebase/programming-elements/UserInputKmToMiles.java
@@ -1,19 +1,10 @@
 import java.util.*;
-
-/**
- * Program to convert kilometers to miles
- * Formula: miles = km / 1.60934
- */
 public class UserInputKmToMiles {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        // Prompt user for input
         System.out.print("Enter distance in kilometers: ");
         double km = sc.nextDouble();
-        
-        // More precise conversion factor
-        double miles = km / 1.60934;
+        double miles = km / 1.6; // Conversion Factor
         
         System.out.println("The total miles is " + miles + " miles for the given " + km + " km");
         
