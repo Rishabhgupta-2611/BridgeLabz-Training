f164d74bba5ac6b8ac4ebdea3c4b7fcaec9faefa
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2025-12-24 11:58:20 +0530

    [Rishabh] Add.swap

---
+++ review/Number.java
@@ -0,0 +1,15 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		int a = 13;
+		int b=8;
+		 a = a^b;
+		 b = a^b;
+	    a=a^b;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
