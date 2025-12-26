c4859842e4f2a7d9fd52e8d40a157fb1791fb007
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2025-12-26 15:59:57 +0530

    [Rishabh] Add.Remove Specific Character

---
+++ core-java-practice/gcr-codebase/extras/RemoveSpecificCharacter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RemoveSpecificCharacter {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		char remove = sc.next().charAt(0);
+
+		String result = "";
+		for (char ch : str.toCharArray()) {
+			if (ch != remove)
+				result += ch;
+		}
+
+		System.out.println("Modified String: " + result);
+		sc.close();
+	}
+}
