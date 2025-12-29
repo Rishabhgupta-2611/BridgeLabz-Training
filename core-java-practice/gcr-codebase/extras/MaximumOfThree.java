3d4c8e8dd56f8db41638cf847a3480228c0d0aa4
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2025-12-29 16:46:43 +0530

    [Rishabh] Add.Maximum of Three

---
+++ core-java-practice/gcr-codebase/extras/MaximumOfThree.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class MaximumOfThree {
+
+	static int findMaximum(int a, int b, int c) {
+		if (a >= b && a >= c)
+			return a;
+		else if (b >= a && b >= c)
+			return b;
+		else
+			return c;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter first number: ");
+		int a = sc.nextInt();
+
+		System.out.print("Enter second number: ");
+		int b = sc.nextInt();
+
+		System.out.print("Enter third number: ");
+		int c = sc.nextInt();
+
+		int max = findMaximum(a, b, c);
+		System.out.println("Maximum number is: " + max);
+
+		sc.close();
+	}
+}
