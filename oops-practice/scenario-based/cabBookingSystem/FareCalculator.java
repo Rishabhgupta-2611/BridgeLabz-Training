72f901a5dfa7a71ca686e32ac8a4d0d92982f785
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2026-01-07 16:53:11 +0530

    [Rishabh]

--- oops-practice/scenario-based/cabBookingSystem/FareCalculator.java
+++
@@ -1,5 +0,0 @@
-package cabBookingSystem;
-
-interface FareCalculator {
-	double calculateFare(double distance);
-}
--- oops-practice/scenario-based/cabBookingSystem/PeakFare.java
+++
@@ -1,9 +0,0 @@
-package cabBookingSystem;
-
-class PeakFare implements FareCalculator {
-
-	@Override
-	public double calculateFare(double distance) {
-		return distance * 15; // ₹15 per km (peak hours)
-	}
-}
