ceb615931788955c6a0fcfff24518210facc57b5
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2026-01-14 12:23:24 +0530

    [Rishabh] Add.Library Management System

---
+++ oops-practice/scenario-based/libraryManagementSystem/FineCalculator.java
@@ -0,0 +1,5 @@
+package libraryManagementSystem;
+
+interface FineCalculator {
+	double calculateFine(int lateDays);
+}
---
+++ oops-practice/scenario-based/libraryManagementSystem/User.java
@@ -0,0 +1,11 @@
+package libraryManagementSystem;
+
+class User {
+	protected String userId;
+	protected String name;
+
+	public User(String userId, String name) {
+		this.userId = userId;
+		this.name = name;
+	}
+}
