60c77fe481c7878db7eee1ba1bcf6cef7473b1e1
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2026-01-22 12:35:52 +0530

    [Rishabh] Add.Search for a Specific Word in a List of Sentences

---
+++ dsa-practice/gcr-codebase/linear-and-binary-search/SearchWordInSentence.java
@@ -0,0 +1,15 @@
+public class SearchWordInSentence {
+	public static String findSentence(String[] sentences, String word) {
+		for (String sentence : sentences) {
+			if (sentence.contains(word)) {
+				return sentence;
+			}
+		}
+		return "Not Found";
+	}
+
+	public static void main(String[] args) {
+		String[] sentences = { "Java is powerful", "I love programming", "Search algorithms are important" };
+		System.out.println(findSentence(sentences, "algorithms"));
+	}
+}
