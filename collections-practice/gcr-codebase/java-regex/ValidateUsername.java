feba09167f70391653ab651a04573e64229ca0f4
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2026-01-30 23:45:57 +0530

    [Rishabh] Add.Validate a Username

---
+++ collections-practice/gcr-codebase/java-regex/ValidateUsername.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ValidateUsername {
+
+	public static boolean isValidUsername(String username) {
+		return username.matches("^[A-Za-z][A-Za-z0-9_]{4,14}$");
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String username = sc.nextLine();
+
+		if (isValidUsername(username)) {
+			System.out.println("Valid");
+		} else {
+			System.out.println("Invalid");
+		}
+		sc.close();
+	}
+}
