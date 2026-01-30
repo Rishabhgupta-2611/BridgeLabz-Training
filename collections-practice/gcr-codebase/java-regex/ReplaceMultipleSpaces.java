3ca54ffa944510fc4e767129921d7f9ded56888f
Rishabhgupta-2611 <rishabh9807114498@gmail.com>
2026-01-30 23:50:53 +0530

    [Rishabh] Add.Replace Multiple Spaces with a Single Space

---
+++ collections-practice/gcr-codebase/java-regex/ReplaceMultipleSpaces.java
@@ -0,0 +1,8 @@
+
+public class ReplaceMultipleSpaces {
+	public static void main(String[] args) {
+		String text = "This   is   an   example   with   multiple   spaces.";
+		text = text.replaceAll("\\s+", " ");
+		System.out.println(text);
+	}
+}
